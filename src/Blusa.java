385d40a84c78b4651ad103b0a28e8a0c0f36dd7c
erich <erichs890@gmail.com>
2023-11-27 10:20:42 -0300

    mudei umas coisinhas

--- src/Blusa.java
+++ src/Blusa.java
@@ -1,2 +1,25 @@
 public class Blusa extends PecaVestuario{
+    public Blusa(float preco, String nome, String marca, String codigo, String material, char genero, String tamanho) {
+        super(preco, nome, marca, codigo, material, genero, tamanho);
+    }
+
+    @Override
+    public double calcularFrete() {
+        return 0;
+    }
+
+    @Override
+    public double prazoValidadeEmDias() {
+        return 0;
+    }
+
+    @Override
+    public boolean estaValido() {
+        return false;
+    }
+
+    @Override
+    public boolean verificarEstoque() {
+        return false;
+    }
 }
